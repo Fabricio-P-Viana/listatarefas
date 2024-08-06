f91cf60676d6f004a8d2973294c221a6f247abbd
Fabricio Viana <fabriciopviana@outlook.com>
2024-08-06 12:02:04 -0300

    Update and save in LocalStorage

--- src/components/Main.js
+++ src/components/Main.js
@@ -12,25 +12,54 @@ export default class Main extends Component {
     state = {
         novaTarefa:'',
         tarefas: [],
+        index: -1,
     };
+
+    componentDidMount(){ // é executado quando o componente é criado
+        const tarefas = JSON.parse(localStorage.getItem('tarefas'));
+
+        if(!tarefas) return;
+
+        this.setState({
+            tarefas,
+        });
+    }
+    componentDidUpdate(prevProps,prevState){ // é executado quando atualzar o componente
+        // prevState pega um estado anterior do atual
+        const { tarefas } = this.state;
+        // podemos comparar esse estados
+        if (tarefas === prevState.tarefas) return;
+
+        localStorage.setItem('tarefas',JSON.stringify(tarefas)); // salvando no localstorage
+    }
 
     handleSubmit = (e) => {
         e.preventDefault();
-        const {tarefas} = this.state;
+        const {tarefas,index} = this.state;
         let {novaTarefa} = this.state;
         novaTarefa = novaTarefa.trim();
 
         if (tarefas.indexOf(novaTarefa) !== -1) return;
+
         const novasTarefas = [...tarefas];
 
-        this.setState({
+        if (index === -1) { // se for nova tarefa
+           this.setState({
             tarefas: [...novasTarefas,novaTarefa],
-        });
+            novaTarefa: '',
+        }); 
+        } else { // estado de edição de tarefa existente
+            novasTarefas[index] = novaTarefa;
+            this.setState({
+                tarefas: [...novasTarefas],
+                index: -1,
+            })
+        }
     }
 
     handleChange = (e) => {
         this.setState({
-            novaTarefa: e.target.value
+            novaTarefa: e.target.value,
         });
     }
 
@@ -42,8 +71,14 @@ export default class Main extends Component {
             tarefas: [...novasTarefas],
         })
     }
+
     handleEdit = (e,index) =>{
+        const {tarefas} = this.state;
         
+        this.setState({
+            index,
+            novaTarefa: tarefas[index],
+        })
     }
 
     render(){
